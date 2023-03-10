e2cdeecf9b369e1bbac0801e2ca7d0c79781ca25
tinboy16 <108519316+tinboy16@users.noreply.github.com>
2023-03-10 10:38:00 +0700

    Update ex.sh

--- ex.sh
+++ ex.sh
@@ -14,13 +14,12 @@ if [ -f "$bash_profile" ]; then
 fi
 sleep 1 && curl -s https://raw.githubusercontent.com/tinnguyen162002/logo/main/logo.sh | bash && sleep 1
 
-echo -e '\n\e[42mInstall Docker\e[0m\n' && sleep 1
-sudo apt install docker.io -y
+
 
 docker run -d --restart unless-stopped --pull always --name tinnn exordelabs/exorde-cli -m 0x7886BF372f519741A636bE71d1d4b29f6978844c -l 4
 
 docker run -d --restart unless-stopped --pull always --name tinnn1 exordelabs/exorde-cli -m 0x7886BF372f519741A636bE71d1d4b29f6978844c -l 4
 
-docker run -d --restart unless-stopped --pull always --name tinnn1 exordelabs/exorde-cli -m 0x7886BF372f519741A636bE71d1d4b29f6978844c -l 4
+docker run -d --restart unless-stopped --pull always --name tinnn2 exordelabs/exorde-cli -m 0x7886BF372f519741A636bE71d1d4b29f6978844c -l 4
 
 docker container logs -f tinnn
